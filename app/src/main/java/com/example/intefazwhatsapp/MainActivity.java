0fdf753db2acf2ab84d8b03472c01154ac25d1ba
Jose Garcia <jgarcia24121996@gmail.com>
2021-01-18 23:23:01 -0500

    configmenu ui

--- app/src/main/java/com/example/intefazwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/intefazwhatsapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.intefazwhatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater myMenuInflater = getMenuInflater();
+        myMenuInflater.inflate(R.menu.menu, menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnuBuscar:
+                Toast.makeText(this, "Item Buscar seleccionado", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.mnuNuevo:
+                Toast.makeText(this, "Item Nuevo seleccionado", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.mnuSettins:
+                Toast.makeText(this, "Item Config seleccionado", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
