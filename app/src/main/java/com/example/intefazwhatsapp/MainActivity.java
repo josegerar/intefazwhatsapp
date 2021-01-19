091038d3c1003b04e99d823bfe573683ced433a4
Jose Garcia <jgarcia24121996@gmail.com>
2021-01-19 06:28:40 -0500

    commit final

--- app/src/main/java/com/example/intefazwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/intefazwhatsapp/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "View open", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
